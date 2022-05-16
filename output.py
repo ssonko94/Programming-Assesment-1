b716f84bbb32f3da25a1aeb9569499743decac52
Jimmy@devs <ssonkojimmy@gmail.com>
2022-05-16 22:27:39 +0300

    Add a function that outputs values of a file from a csv file.
    import the csv file creator to create a csv file and write to it using a list of string values.
    
    Use a try exepection to only read content after a file has been created sucessfully and then call the file reader.
    
    Read the values and print them to the screen. Print any errrors if the file file fails to be created for some reason

---
+++ output.py
@@ -0,0 +1,21 @@
+from csv_creator import csvCreator
+from csv_content_file_reader import csv_content_file_reader
+
+
+def output(file_name: str = 'example.csv', data: list = ['honey', 'milk', 'cheese']) -> None:
+    """
+        @param file_name: name of the file to be created
+        @param data: list of data to be written to the file
+        Creates a csv file from a list of strings.
+    """
+    try:
+        csvCreator(file_name, data)
+        print('Success!')
+        print(csv_content_file_reader(file_name))
+    except Exception as e:
+        print(e)
+        print('Error!')
+
+
+output('cars.csv', ['Rolls Royce', 'Volkswagen', 'Aston Martin',
+       'Audi', 'BMW', 'Mercedes-Benz', 'Porsche', 'Bentley', 'Lamborghini', 'Bugatti', 'Ferrari'])
