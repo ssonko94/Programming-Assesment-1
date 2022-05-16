06b0f9e5590a619349eef846f1e7f96fbc855f1d
Jimmy@devs <ssonkojimmy@gmail.com>
2022-05-16 22:22:08 +0300

    create a file reader function that takes in the file name and reads it's content.
    with the file open loop through the csv file and return a list of the values.
    
    Using a list comprehension create an array of the values for each of the value separated by commas.

---
+++ csv_content_file_reader.py
@@ -0,0 +1,12 @@
+import csv
+
+
+def csv_content_file_reader(file_name: str) -> list:
+    '''
+        @param file_name: name of the file to be read
+        @return: list of strings from the file
+        Reads a csv file and returns a list of strings.
+    '''
+    with open(file_name, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        return [row for row in reader]
